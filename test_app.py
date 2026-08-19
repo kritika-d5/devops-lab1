29e2ec0b709fa8144cfbc19cf3c0ff18c209339d
kritika-d5 <kritikadubey2005@gmail.com>
2026-08-19 15:50:09 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 from app import add, greet
 
 def test_add():
-    assert add(2, 3) == 99 # deliberate failure
-
+    assert add(2, 3) == 5
+    
 
 def test_greet():
     assert greet("World") == "Hello, World!"
